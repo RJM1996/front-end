32b959689a776d3fddbf5b84db9b50acbab5fc3f
rongjunming <rongjunming@rong360.com>
2021-07-28 16:56:23 +0800

    ✨ feat(单文件上传):

---
+++ demo/file-upload/client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <SingleFileUpload />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SingleFileUpload from './components/SingleFileUpload.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SingleFileUpload
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/file-upload/client/src/components/SingleFileUpload.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="SingleFileUpload">
+    <h1>单文件上传</h1>
+    <div>
+      <input id="uploadFile" type="file" accept="image/*" />
+      <button id="submit" @click="uploadFile()">上传文件</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'SingleFileUpload',
+
+  components: {},
+
+  props: {},
+
+  data () {
+    return {
+    }
+  },
+
+  computed: {},
+
+  methods: {
+    async uploadFile () {
+      const uploadFileEle = document.querySelector('#uploadFile')
+      console.log(111, uploadFileEle, uploadFileEle.files)
+      if (!uploadFileEle.files.length) return
+      const file = uploadFileEle.files[0] // 获取单个文件
+      // 省略文件的校验过程，比如文件类型、大小校验
+      this.upload({
+        url: '/single',
+        file
+      })
+    },
+
+    upload ({ url, file, fieldName = 'file' }) {
+      const request = axios.create({
+        baseURL: 'http://localhost:3000/upload',
+        timeout: 60000
+      })
+      let formData = new FormData()
+      formData.set(fieldName, file)
+      console.log(formData)
+      request
+        .post(url, formData, {
+          // 监听上传进度
+          onUploadProgress: function (progressEvent) {
+            const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total)
+            console.log('进度', percentCompleted)
+          }
+        })
+        .then((result) => {
+          console.log(result.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+
+  created () { },
+}
+
+</script>
+
+<style lang='less' scoped>
+#SingleFileUpload {
+  width: 500px;
+  padding: 20px;
+  border: 1px solid;
+}
+</style>
