a8ce549cc8c4c47114c54d0420226045e0d34c73
rongjunming <rjm96@foxmail.com>
2021-08-04 18:59:41 +0800

    ✨ feat(大文件分片上传, 合并文件):

--- demo/file-uploadAndDownload/server/app.js
+++ demo/file-uploadAndDownload/server/app.js
@@ -1,14 +1,18 @@
 const path = require('path')
 const Koa = require('koa')
+const util = require('util')
 const serve = require('koa-static')
 const cors = require('@koa/cors')
 const multer = require('@koa/multer')
 const Router = require('@koa/router')
 const fse = require('fs-extra')
+const fs = require('fs')
+const readdir = util.promisify(fs.readdir)
+const unlink = util.promisify(fs.unlink)
 
 const app = new Koa()
 const router = new Router()
-const PORT = 3000
+const PORT = 8080
 // 上传后资源的URL地址
 const RESOURCE_URL = `http://localhost:${PORT}`
 // 存储上传文件的目录
@@ -21,7 +25,7 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     // 设置文件名
-    cb(null, file.fieldname + Date.now())
+    cb(null, file.originalname)
   }
 })
 
@@ -152,30 +156,110 @@ router.post(
   ])
 )
 
+const TMP_DIR = path.join(__dirname, '/public/upload') // 临时目录
+const bigFileStorage = multer.diskStorage({
+  destination: async function (req, file, cb) {
+    let fileMd5 = file.originalname.split('-')[0]
+    const fileDir = path.join(TMP_DIR, fileMd5)
+    await fse.ensureDir(fileDir)
+    cb(null, fileDir)
+  },
+  filename: function (req, file, cb) {
+    let chunkIndex = file.originalname.split('-')[1]
+    cb(null, `${chunkIndex}`)
+  }
+})
+
+const multerBigFileUpload = multer({ storage: bigFileStorage })
 // 大文件分片上传
-router.post(
-  '/upload/bigFile',
-  async (ctx, next) => {
-    try {
-      await next()
-      // console.log('file: ', ctx.file)
-      ctx.body = {
-        code: 1,
-        msg: '文件上传成功',
-        // url: `${RESOURCE_URL}/${ctx.file.originalname}`,
-        ctx
-      }
-    } catch (error) {
-      console.log(error)
-      ctx.body = {
-        code: 0,
-        msg: '文件上传失败',
-        ctx: ctx
+router.post('/upload/bigFile', multerBigFileUpload.single('file'), async (ctx, next) => {
+  try {
+    ctx.body = {
+      code: 1,
+      msg: '分片上传成功',
+      data: ctx.file
+    }
+  } catch (error) {
+    console.log(error)
+    ctx.body = {
+      code: 0,
+      msg: '分片上传失败',
+      data: error
+    }
+  }
+})
+// mergeChunks
+router.get('/upload/mergeChunks', async (ctx) => {
+  const { fileName, token } = ctx.query
+  await mergeFiles(fileName, token)
+  ctx.body = {
+    code: 200,
+    data: {
+      url: `http://localhost:${PORT}/${fileName}`
+    }
+  }
+})
+
+async function mergeFiles(fileName, token) {
+  // 存储分片的目录
+  const sourceDir = path.join(TMP_DIR, token)
+  // 合成的文件存放的路径
+  const targetPath = path.join(UPLOAD_DIR, fileName)
+  // 1. 定义一个函数用来读取文件,并向一个地址写入文件
+  function readFile(file, ws) {
+    new Promise((resolve, reject) => {
+      fs.createReadStream(file)
+        .on('data', (data) => ws.write(data))
+        .on('end', resolve)
+        .on('error', reject)
+    })
+  }
+  // 2. 读取存放分片的目录
+  const files = await readdir(sourceDir)
+  const sortFiles = files.sort((a, b) => a - b)
+  // 3. 根据目标路径创建一个写文件流,往进写文件
+  const ws = fs.createWriteStream(targetPath)
+  // 4. 遍历分片文件, 依次写入目标文件
+  for (const file of sortFiles) {
+    let filePath = path.join(sourceDir, file)
+    await readFile(filePath, ws)
+    // await unlink(filePath)
+  }
+  ws.end()
+}
+
+// 判断文件是否存在
+router.get('/upload/exist', async (ctx) => {
+  const { fileName, token } = ctx.query
+  const filePath = path.join(UPLOAD_DIR, fileName)
+  const isExists = await fse.pathExists(filePath)
+  console.log('存在: ', isExists)
+  if (isExists) {
+    ctx.body = {
+      status: 'success',
+      data: {
+        isExist: true,
+        url: `http://localhost:8080/${fileName}`
       }
     }
-  },
-  multerUpload.single('file')
-)
+  } else {
+    let chunkIds = []
+    const chunksPath = path.join(TMP_DIR, token)
+    const hasChunksPath = await fse.pathExists(chunksPath)
+    if (hasChunksPath) {
+      let files = await readdir(chunksPath)
+      chunkIds = files.map((f) => Number(f))
+      console.log('chunkIds', chunkIds)
+    }
+    ctx.body = {
+      status: 'success',
+      data: {
+        isExist: false,
+        chunkIds
+      }
+    }
+  }
+})
 
 // 注册中间件
 app.use(cors())
